e622774bd4e0202ee1430f2ce3c6efa2d867bac3
Leonid Solovev <ls3112@yandex.ru>
2023-04-16 17:15:38 +0300

    Task1 done

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int num1;
+while (true)
+{
+        Console.Write("Введите чило от 100 до 999: ");
+        if (int.TryParse(Console.ReadLine(), out num1))
+        if (num1 > 99)
+        if (num1 < 1000)
+              break;
+        Console.WriteLine("Ошибка ввода!");
+}
+
+int num2 = (num1/10)%10;
+Console.WriteLine($"{num1} -> {num2}");
